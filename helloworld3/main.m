e60f949efe5cb73603156da213cb8c8e31e3a11d
Mark Cargill <markcargill@Marks-MacBook-Pro.local>
2014-04-24 18:25:00 -0400

    Initial Commit

---
+++ helloworld3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  helloworld3
+//
+//  Created by Mark Cargill on 4/24/14.
+//  Copyright (c) 2014 Mark Cargill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
